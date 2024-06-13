eea35668ce62d0729347b135e96f8a2bb02b60b7
fren43051 <32681314+fren43051@users.noreply.github.com>
2024-06-12 19:47:57 -0600

    funciona segunda prueba

--- api/src/main/java/clinic/voll/api/controller/MedicoController.java
+++ api/src/main/java/clinic/voll/api/controller/MedicoController.java
@@ -1,6 +1,10 @@
 package clinic.voll.api.controller;
 
 import clinic.voll.api.medico.DatosRegistroMedico;
+import clinic.voll.api.medico.Medico;
+
+import clinic.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository medicoRepository;
+
     @PostMapping
-    public void registrarMedico(@RequestBody DatosRegistroMedico datosRedistroMedico) {
-        System.out.println(datosRedistroMedico);
+    public void registrarMedico(@RequestBody DatosRegistroMedico datosRegistroMedico) {
+        medicoRepository.save(new Medico(datosRegistroMedico));
     }
 }
---
+++ api/src/main/java/clinic/voll/api/direccion/Direccion.java
@@ -0,0 +1,26 @@
+package clinic.voll.api.direccion;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Direccion {
+    private String calle;
+    private String numero;
+    private String complemento;
+    private String distrito;
+    private String ciudad;
+
+    public Direccion(DatosDireccion direccion) {
+        this.calle = direccion.calle();
+        this.numero = String.valueOf(direccion.numero());
+        this.distrito = direccion.distrito();
+        this.complemento = direccion.complemento();
+        this.ciudad = direccion.ciudad();
+    }
+}
